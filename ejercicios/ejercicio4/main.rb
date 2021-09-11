0fc034fa91d7a3f601571a6fb6be5ca5a0ba108f
Havith90 <javiera.segurav@gmail.com>
2021-09-10 21:20:32 -0300

    Ejercicio módulo menú de main a nuevo archivo "modulo_menu.rb"

--- ejercicios/ejercicio4/main.rb
+++ ejercicios/ejercicio4/main.rb
@@ -6,8 +6,36 @@ include ModuloNumeros #acá el nombre debe ir tal cual q en elarchivo con mayusc
 include ModuloUsuarios                  # incluye el módulo usuarios
 include ModuloImpresiones               # incluye el módulo impresiones
 
-arreglo_usuarios_correo_faker = generar_usuario_con_correo(5)   # módulo usuarios
-imprimir_usuarios_con_correos(arreglo_usuarios_correo_faker)    # módulo impresiones
+module ModuloMenu
 
-arreglo_usuario_con_gustos = generar_usuarios_con_gustos(5)
-imprimir_usuarios_con_gustos(arreglo_usuario_con_gustos)
+    def mostrar_menu()
+        opcion = 0
+        while opcion != 3
+            system("clear")
+            puts "Generador de datos".center(50,"-")
+            puts "Ingrese la opción que desea:"
+            puts "1. Generar lista de usuarios con correo"
+            puts "2. Generar lista de usuarios con gustos"
+            puts "3. Salir"
+
+            opcion = gets.chomp.to_i
+
+            if opcion == 1
+                puts "Introduzca la cantidad de registros"
+                cantidad = gets.chomp.to_i
+
+                arreglo_usuarios_correo_faker = generar_usuario_con_correo(5)   # módulo usuarios
+                imprimir_usuarios_con_correos(arreglo_usuarios_correo_faker)    # módulo impresiones
+            elsif opcion == 2
+                puts "Introduzca cantidad de registros"
+                cantidad = gets.chomp.to_i
+
+                arreglo_usuario_con_gustos = generar_usuarios_con_gustos(5)
+                imprimir_usuarios_con_gustos(arreglo_usuario_con_gustos)
+            elsif opcion == 3
+                puts "Saliendo del generador de datos"
+            else
+                puts "opción no válida"
+            end
+                puts "Presione ENTER para continuar"
+            gets
---
+++ ejercicios/ejercicio4/modulo_menu.rb
@@ -0,0 +1,6 @@
+#esto va al main y lo del main traerlo para acá completo
+
+require_relative 'modulo_menu'
+include modulo_menu
+
+mostrar_menu()
